0aa25c172dbbb10ea5ad6f1ce7c5731d993ae943
shahansv <shahanvsaleem@gmail.com>
2026-01-14 10:03:32 +0530

    payment success and failed design completed

--- src/components/PaymentFailed.jsx
+++ src/components/PaymentFailed.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Header from './Header';
 
 const PaymentFailed = () => {
   return (
-    <div>PaymentFailed</div>
-  )
+    <>
+      <Header />
+
+      <div className="h-96 flex flex-col justify-center items-center">
+        <img src="failed.gif" alt="" className="h-60" />
+        <h1 className="text-3xl text-red-400 font-extrabold">
+          Payment Failed
+        </h1>
+      </div>
+    </>
+  );
 }
 
 export default PaymentFailed
--- src/components/PaymentSuccess.jsx
+++ src/components/PaymentSuccess.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import Header from "./Header";
 
 const PaymentSuccess = () => {
   return (
-    <div>PaymentSuccess</div>
-  )
-}
+    <>
+      <Header />
 
-export default PaymentSuccess
+      <div className="h-96 flex flex-col justify-center items-center">
+        <img src="success.gif" alt="" className="h-60" />
+        <h1 className="text-3xl text-green-400 font-extrabold">Payment Success</h1>
+      </div>
+    </>
+  );
+};
+
+export default PaymentSuccess;
